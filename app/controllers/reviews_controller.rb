25b8c3bc8df9d4049c4b5c420b2af6625b1fdae5
mehdi ayoub <ayoub.mehdi@outlook.com>
2023-08-23 17:17:20 +0400

    Done

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -28,7 +28,7 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:rating, :content)
   end
 
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   validates :content, presence: true
-  validates :rating, numericality: { in: 0..5 }, numericality: { only_integer: true }, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, presence: true
 end
